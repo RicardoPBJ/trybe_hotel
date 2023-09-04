4297d2f5f0089b16301f2463ffd48bd77e99e40b
Ricardo <rbrumjunio@gmail.com>
2023-09-03 23:06:54 -0300

    Desenvolva o endpoint GET /room/:hotelId

--- src/TrybeHotel/Repository/RoomRepository.cs
+++ src/TrybeHotel/Repository/RoomRepository.cs
@@ -14,7 +14,21 @@ namespace TrybeHotel.Repository
         // 6. Desenvolva o endpoint GET /room/:hotelId
         public IEnumerable<RoomDto> GetRooms(int HotelId)
         {
-            throw new NotImplementedException(); 
+            return _context.Rooms.Select(room => new RoomDto
+            {
+                roomId = room.RoomId,
+                name = room.Name,
+                capacity = room.Capacity,
+                image = room.Image,
+                hotel = new HotelDto
+                {
+                    hotelId = room.Hotel!.HotelId,
+                    name = room.Hotel.Name,
+                    address = room.Hotel.Address,
+                    cityId = room.Hotel.City!.CityId,
+                    cityName = room.Hotel.City!.Name,
+                }
+            }).ToList();
         }
 
         // 7. Desenvolva o endpoint POST /room
